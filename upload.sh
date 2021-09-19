9a513cf271883daa293e62eb7611498340947847
xuyunquan <xuyq456@163.com>
2021-09-19 15:19:57 +0800

    no code

--- upload.sh
+++
@@ -1,6 +0,0 @@
-# first run myvenv to go into the python virtual environment, then run deactivate to exit when finished
-aws s3 cp ~/projects/idratherbewriting.io/images/$1 s3://idbwmedia.com/images/ --profile wasabi
-# to run, store image in project's image folder
-# then type this: . upload.sh image.png
-echo '<a href=""><img src="https://s3.us-west-1.wasabisys.com/idbwmedia.com/images/'$1
-echo '" alt="" /></a>'
